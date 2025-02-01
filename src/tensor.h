67d240ccb049eb1bcd4b790429f04b3a722ae869
DaengDo <ringyoon567@gmail.com>
2025-02-01 13:08:27 +0900

    feat: support `tensor` multiplication and division

--- src/tensor.h
+++ src/tensor.h
@@ -9,6 +9,13 @@ class Tensor {
   std::vector<double> buffer;
   std::vector<int> shape;
 
+  static bool is_same_dimension(const Tensor& t1, const Tensor& t2) {
+    return t1.shape.size() == t2.shape.size();
+  }
+
+  bool is_scalar() const { return shape.size() == 0 && buffer.size() == 1; }
+  bool is_matrix() const { return shape.size() == 2; }
+
  public:
   // scalar
   Tensor(const double& scalar) : buffer{scalar}, shape{} {}
@@ -46,7 +53,7 @@ class Tensor {
   }
 
   Tensor operator+(const Tensor& other) const {
-    if (other.shape != this->shape) {
+    if (!is_same_dimension(*this, other)) {
       // TODO: change to compile time error
       std::runtime_error("Attempted addition between incompatible vector spaces.");
     }
@@ -62,7 +69,7 @@ class Tensor {
   }
 
   Tensor operator-(const Tensor& other) const {
-    if (other.shape != this->shape) {
+    if (!is_same_dimension(*this, other)) {
       // TODO: change to compile time error
       std::runtime_error("Attempted addition between incompatible vector spaces.");
     }
@@ -72,6 +79,93 @@ class Tensor {
     result.reserve(buffer_size);
     for (int i = 0; i < buffer_size; i++) {
       result.push_back(buffer[i] - other.buffer[i]);
+    }
+
+    return Tensor(shape, result);
+  }
+
+  // handle scalar multiplication
+  template <typename T, typename = std::is_arithmetic<T>>
+  Tensor operator*(const T scalar) const {
+    auto buffer_size = buffer.size();
+    std::vector<double> result;
+    result.reserve(buffer_size);
+    for (int i = 0; i < buffer_size; i++) {
+      result.push_back(buffer[i] * static_cast<double>(scalar));
+    }
+
+    return Tensor(shape, result);
+  }
+
+  Tensor operator*(const Tensor other) const {
+    // handle scalar multiplication
+    if (other.is_scalar()) {
+      auto buffer_size = buffer.size();
+      std::vector<double> result;
+      result.reserve(buffer_size);
+      auto scalar_value = other.buffer[0];
+      for (int i = 0; i < buffer_size; i++) {
+        result.push_back(buffer[i] * scalar_value);
+      }
+
+      return Tensor(shape, result);
+    }
+
+    // TODO: implement broadcasting multiplication
+    if (!is_same_dimension(*this, other)) {
+      std::runtime_error("broadcasting multiplication is not supported");
+    }
+
+    // handle element-wise multiplication
+    auto buffer_size = buffer.size();
+    std::vector<double> result;
+    result.reserve(buffer_size);
+    for (int i = 0; i < buffer_size; i++) {
+      result.push_back(buffer[i] * other.buffer[i]);
+    }
+
+    return Tensor(shape, result);
+  }
+
+  // handle scalar divide
+  template <typename T, typename = std::is_arithmetic<T>>
+  Tensor operator/(const T scalar) {
+    auto buffer_size = buffer.size();
+    std::vector<double> result;
+    result.reserve(buffer_size);
+    for (int i = 0; i < buffer_size; i++) {
+      result.push_back(buffer[i] / static_cast<double>(scalar));
+    }
+
+    return Tensor(shape, result);
+  }
+
+  Tensor operator/(const Tensor other) const {
+    // handle scalar division
+    if (other.is_scalar()) {
+      auto buffer_size = buffer.size();
+      std::vector<double> result;
+      result.reserve(buffer_size);
+      auto scalar_value = other.buffer[0];
+      for (int i = 0; i < buffer_size; i++) {
+        result.push_back(buffer[i] / scalar_value);
+      }
+
+      return Tensor(shape, result);
+    }
+
+    // TODO: implement broadcasting division
+    if (!is_same_dimension(*this, other)) {
+      std::runtime_error("broadcasting multiplication is not supported");
+    }
+
+    // handle element-wise division
+    auto buffer_size = buffer.size();
+    std::vector<double> result;
+    result.reserve(buffer_size);
+    for (int i = 0; i < buffer_size; i++) {
+      // TODO: handle division with 0
+      result.push_back(buffer[i] / other.buffer[i]);
     }
 
     return Tensor(shape, result);
